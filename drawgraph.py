93d988eeb98de88040247e91c15f89f644a101ca
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-07-15 18:58:00 +0900

    Add files via upload

--- drawgraph.py
+++ drawgraph.py
@@ -2,9 +2,9 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-screen.pygame.display.set_mode((500,500))
+screen=pygame.display.set_mode((500,500))
 color = (255,255,255)
-fps = pygame.time.clock()
+fps = pygame.time.Clock()
 
 px = -50
 x = -49
@@ -14,12 +14,17 @@ y = x ** 2 + 2 * x + 3
 def makeGraph():
     global px,x,py,y
 
-    for i in range(100):
-        px += 1
-        x += 1
-        py = px ** 2 + 2 * px + 3
-        y = x ** 2 + 2 * x + 3
-        pygame.draw.line(screen, (0,0,0), (px + 50,py), (x + 50,y), 5)
+    px = -50
+    x = -49
+    py = px ** 2 + 2 * px + 3
+    y = x ** 2 + 2 * x + 3
+
+    for i in range(10000):
+        px += 0.01
+        x += 0.01
+        py = 500 - (px ** 2 + 2 * px + 3)
+        y = 500 - (x ** 2 + 2 * x + 3)
+        pygame.draw.line(screen, (0,0,0), (px + 250,py), (x + 250,y), 5)
 
 while True:
     for event in pygame.event.get():
